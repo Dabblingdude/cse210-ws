2547d6f1778eebe3318c833a21f2289be7796e4c
SNiemand <samniemand1221@gmail.com>
2025-01-16 19:24:42 -0800

    did prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Please enter a series of numbers. Enter '0' when you would like to stop.");
+
+        List<int> numbers = new List<int>();
+        int newNum = 1;
+
+        while (newNum != 0)
+        {
+        newNum = int.Parse(Console.ReadLine());
+
+        if (newNum != 0)
+        {
+            numbers.Add(newNum);
+        }
+        }
+
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
     }
 }
